247cea734c6d9abcdeacc0761a4143200f99bde1
Dzmitry Navumau <dzmitriy.naumov@gmail.com>
2020-07-15 16:18:25 +0300

    Add FileCabinetRecord.cs file

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetRecord
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
