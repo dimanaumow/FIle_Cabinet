66f60e0eb1d4c17b93c21b88ec911ca2b6976986
Dzmitry Navumau <dzmitriy.naumov@gmail.com>
2020-07-15 16:33:05 +0300

    Add FileCabinetService.cs file

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>;
+
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            //TODO: add realization
+
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            //TODO: add realization 
+            return new FileCabinetRecord[] { };
+        }
+
+        public int GetStat()
+        {
+            //TODO: add realization
+
+            return 0;
+        }
+    }
+}
