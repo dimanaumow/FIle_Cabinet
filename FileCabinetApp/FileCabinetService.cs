f03d782bc0dae352b9b2ad13534d6aad8bd9c41f
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-10-07 16:12:43 +0300

    Add guard clauses to CreateRecord method

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -8,9 +8,48 @@ namespace FileCabinetApp
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
 
-        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short expirience,
-            decimal balance, char nationality)
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short expirience, decimal balance, char nationality)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                if (firstName is null)
+                {
+                    throw new ArgumentNullException($"{nameof(firstName)} cannot be null.");
+                }
+
+                if (firstName.Length < 2 || firstName.Length > 60)
+                {
+                    throw new ArgumentException($"{nameof(firstName.Length)} must be in range 2 to 60.");
+                }
+
+                throw new ArgumentException($"{nameof(firstName)} cannot be empty or whiteSpace.");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                if (lastName is null)
+                {
+                    throw new ArgumentNullException($"{nameof(lastName)} cannot be null.");
+                }
+
+                if (lastName.Length < 2 || lastName.Length > 60)
+                {
+                    throw new ArgumentException($"{nameof(lastName.Length)} must be in range 2 to 60.");
+                }
+
+                throw new ArgumentException($"{nameof(lastName)} cannot be empty or whiteSpace.");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} is incorrect.");
+            }
+
+            if (expirience < 0 || expirience > DateTime.Now.Year - dateOfBirth.Year)
+            {
+                throw new ArgumentException($"{nameof(expirience)} must be positive and less than year of life.");
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -18,7 +57,7 @@ namespace FileCabinetApp
                 LastName = lastName,
                 DateOfBirth = dateOfBirth,
                 Expirience = expirience,
-                Balance = balance,
+                Balance = balance, 
                 Nationality = nationality
             };
 
